4d1c20dc77b52ddd14f4a3c249c387c5c6c89cfd
Thomas <74390441+ThomRib@users.noreply.github.com>
2024-05-13 09:08:06 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -3,12 +3,12 @@
 // sesi dimulai
 session_start();
 
-// supaya panggil tidak menyertakan mysqli_query($con, sql perintah) lagi, melainkan langsung q();
-function q($q){
-	$ss=new mysqli("localhost","root","","");
+  // supaya panggil tidak menyertakan mysqli_query($con, sql perintah) lagi, melainkan langsung q();
+  function q($q){
+ 	$ss=new mysqli("localhost","root","","");
 	return $ss->query($q);
 	$ss->close();
-}
+  }
 
   // fungsi crud
 
